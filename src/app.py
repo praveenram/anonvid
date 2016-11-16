d35dd8eb69dee95e6e2dc0cd9fda6f8a6e50af0c
Praveenram <praveenram.ba@gmail.com>
2016-11-16 02:00:59 -0500

    Added create / join HTML, started work on backend data model code to store, retrieve conferences with passwords

--- src/app.py
+++ src/app.py
@@ -1,11 +1,34 @@
+"""
+Anonvid Server Script
+"""
+
 from flask import Flask
 from flask import render_template
+from flask import request
+
+from database import Database
 
 app = Flask(__name__)
+db = Database()
 
-@app.route("/")
+
+@app.route("/", methods=['GET'])
+@app.route("/join", methods=['GET'])
 def index():
 	return render_template("index.html")
+
+
+@app.route("/create", methods=['GET'])
+def create():
+	return render_template("create.html")
+
+
+@app.route("/conference", methods=['GET', 'POST'])
+def conference():
+	c_no = request.args.get('c_no')
+	c_p = request.args.get('c_p')
+	return render_template("conference.html", num = c_no)
+
 
 if __name__ == "__main__":
 	app.run(debug = True, port = 8000, host = "0.0.0.0")
---
+++ src/database.py
@@ -0,0 +1,43 @@
+"""
+Anonvid Data model
+"""
+
+import uuid
+import hashlib
+from pymongo import MongoClient
+
+salt = '5d1fa76d7874970190707c55595d32331b113f762447fa49b226e8b471e4145f'
+
+
+class Database(object):
+	def __init__(self, mongo_host='localhost', mongo_port=27017):
+		self._db_client = MongoClient(mongo_host, mongo_port)
+		self._db = self._db_client.anonvid_prod
+
+	def create_conference(self, name, password):
+		conf_id = generate_conf_id()
+		password_hash = generate_password_hash(password)
+
+		conferences = self._db.conferences
+		conference = { 'name': name.strip(), 'conf_id': conf_id, 'password': password_hash }
+		c_id = conferences.insert_one(conference).inserted_id
+
+		if(c_id is None):
+			return -1
+
+		return conf_id
+
+	def find_conference(self, conf_id):
+		conferences = self._db.conferences
+		conf = conferences.find_one({ 'conf_id': conf_id.strip() })
+
+		if conf is not None:
+			return conf
+
+	def generate_conf_id():
+		return uuid.uuid4()
+
+	def generate_password_hash(password):
+		import hashlib, binascii
+		dk = hashlib.pbkdf2_hmac('sha256', password.encode(), salt.encode(), 100000)
+		return binascii.hexlify(dk)
