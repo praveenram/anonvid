b01b233784fda751c948911674a28e3697f3bdc1
Praveenram <praveenram.ba@gmail.com>
2016-11-16 16:36:03 -0500

    Adding code to capture local video / audio stream using webrtc simplewebrtc library

--- src/static/active_conference.js
+++ src/static/active_conference.js
@@ -1,3 +1,15 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    var webrtc = new SimpleWebRTC({
+        // the id/element dom element that will hold "our" video
+        localVideoEl: 'localVideo',
+        // the id/element dom element that will hold remote videos
+        remoteVideosEl: 'remotesVideos',
+        // immediately ask for camera access
+        autoRequestMedia: true
+    });
 
+    webrtc.on('readyToCall', function() {
+        // you can name it anything
+        webrtc.joinRoom('your awesome room name');
+    });
 });
