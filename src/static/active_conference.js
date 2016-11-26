3090dda389963e3a54bec781db4013f03af91143
Praveenram <praveenram.ba@gmail.com>
2016-11-26 14:14:32 -0500

    Added basic text chat functionality to website

--- src/static/active_conference.js
+++ src/static/active_conference.js
@@ -1,10 +1,12 @@
+var debug = false;
+
 $(document).ready(function() {
     var confId = $("#confId").val();
     var username = $("#username").val();
 
     var remoteVideoCount = 0;
 
-    var resizeRemoteVideos = function () {
+    var resizeRemoteVideos = function() {
         //TODO:
         // calculate height and width depending on layout
         // layout depends on the number of videos available to display
@@ -14,6 +16,7 @@ $(document).ready(function() {
     // Web RTC library and events
 
     var webrtc = new SimpleWebRTC({
+        debug: false,
         //Signaling server
         url: "http://localhost:8888/",
         //Video feed
@@ -22,6 +25,8 @@ $(document).ready(function() {
         autoRequestMedia: true,
         //Conference options
         autoRemoveVideos: true,
+        autoAdjustMic: true,
+        detectSpeakingEvents: true,
         adjustPeerVolume: true,
         peerVolumeWhenSpeaking: 0.01,
         media: {
@@ -31,15 +36,15 @@ $(document).ready(function() {
     });
 
     webrtc.on('readyToCall', function() {
-        if(debug == true) {
+        if (debug == true) {
             console.log("Ready to call");
         }
-        webrtc.joinRoom('anonvid-' + confId, function (err, roomDescription) {
-            if(err) {
+        webrtc.joinRoom('anonvid-' + confId, function(err, roomDescription) {
+            if (err) {
                 //TODO: Show error alert - unable to join conference try again later
             }
 
-            if(debug == true) {
+            if (debug == true) {
                 console.log("Join Room");
                 console.log(err);
                 console.log(roomDescription);
@@ -48,21 +53,21 @@ $(document).ready(function() {
     });
 
     webrtc.on('connectionReady', function(sessionId) {
-        if(debug == true) {
+        if (debug == true) {
             console.log("Connection ready");
             console.log(sessionId);
         }
     });
 
     webrtc.on('createdPeer', function(peer) {
-        if(debug == true) {
+        if (debug == true) {
             console.log("Peer created");
             console.log(peer);
         }
     });
 
     webrtc.on('leftRoom', function(roomName) {
-        if(debug == true) {
+        if (debug == true) {
             console.log("leftRoom");
             console.log(roomName);
         }
@@ -71,7 +76,7 @@ $(document).ready(function() {
     webrtc.on('videoAdded', function(videoEl, peer) {
         remoteVideoCount++;
         resizeRemoteVideos();
-        if(debug == true) {
+        if (debug == true) {
             console.log("Video added");
             console.log(videoEl);
             console.log(peer);
@@ -81,49 +86,88 @@ $(document).ready(function() {
     webrtc.on('videoRemoved', function(videoEl, peer) {
         remoteVideoCount--;
         resizeRemoteVideos();
-        if(debug == true) {
+        if (debug == true) {
             console.log("Video removed");
             console.log(videoEl);
             console.log(peer);
         }
     });
 
-    webrtc.on('chatReceived', function (user, message) {
-        // TODO: Sanitize before displaying on the browser
-        // Sanitize for XSS
-    });
-
-
     // Conference window events
-    $(".disable-video").on('click', function () {
+    $(".disable-video").on('click', function() {
         $(".enable-video").removeClass("hidden");
         $(".disable-video").addClass("hidden");
         $("video#localVideo").addClass("hidden");
         // Cut video stream
     });
 
-    $(".enable-video").on('click', function () {
+    $(".enable-video").on('click', function() {
         $(".enable-video").addClass("hidden");
         $(".disable-video").removeClass("hidden");
         $("video#localVideo").removeClass("hidden");
         // Re-enable video stream
     });
 
-    $(".end-conf").on('click', function () {
+    $(".end-conf").on('click', function() {
         // Leave conference
         // Ajax call to server and singnaling server to leave conference
         // Redirect to index page
     });
 
-    $(".enable-microphone").on('click', function () {
+    $(".enable-microphone").on('click', function() {
         $(".enable-microphone").addClass("hidden");
         $(".disable-microphone").removeClass("hidden");
         //Unmute local audio
     });
 
-    $(".disable-microphone").on('click', function () {
+    $(".disable-microphone").on('click', function() {
         $(".enable-microphone").removeClass("hidden");
         $(".disable-microphone").addClass("hidden");
         //Mute local user audio
     });
+
+    //Chat functionality
+    var showMessageOnScreen = function(username, msg) {
+        // Escape characters of HTML / JS from this.
+        console.log(msg);
+        var escapeCharacters = ["&", "<", ">", '"', "'", "/"];
+        var escapeMap = {
+            "&": "&amp;",
+            "<": "&lt;",
+            ">": "&gt;",
+            '"': "&quot;",
+            "'": "&#x27;",
+            "/": "&#x2F;"
+        };
+        for(var i = 0; i < escapeCharacters.length; i++) {
+            msg = msg.replace(escapeCharacters[i], escapeMap[escapeCharacters[i]]);
+        }
+        console.log(msg);
+        $(".message-feed").append("<p>" + username + ": " + msg + "</p>");
+    };
+
+    var sendChatMessageToAll = function(username, msg) {
+        /*
+        sendDirectlyToAll(channelLabel, messageType, payload) - broadcasts a message to all peers in the room via a dataChannel
+            string channelLabel - the label for the dataChannel to send on
+            string messageType - the key for the type of message being sent
+            object payload - an arbitrary value or object to send to peers
+        */
+        webrtc.sendToAll("chatReceived", {
+            user: username,
+            msg: msg
+        });
+    };
+
+    webrtc.on('chatReceived', function(payload) {
+        showMessageOnScreen(payload['user'], payload['msg']);
+    });
+
+    $("button.send-chat").on('click', function() {
+        var msg = $("#chatInput").val().trim();
+        showMessageOnScreen(username, msg);
+        sendChatMessageToAll(username, msg);
+    });
+
+    //Add event to check if enter key is pressed on the #chatInput
 });
